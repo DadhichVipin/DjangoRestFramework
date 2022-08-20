2958d83006337f403461c15007505f6171b9eeb8
vipin <107750@dypatiluniversitypune.edu.in>
2022-08-20 16:11:42 +0530

    DRF5 Project- Validation
    Field Level Validation

---
+++ DRF5/crud/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Student
+
+"""
+we will use third party application to perform operation 
+in this case we will use myapp.py file 
+
+
+***** Field level Validation *****
+"""
+
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
+
+    # Field Level Validation
+    def validate_roll(self, value):
+        if value >= 200:
+            raise serializers.ValidationError("Seat Full... ")
+        return value
